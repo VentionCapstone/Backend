6032555cef2f9f57891ba14c60be5dc9fa2cfb49
Zach13131 <125944644+Zach13131@users.noreply.github.com>
2023-12-03 15:57:36 +0500

    fix(update): swagger only in development

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  if (process.env.MODE !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Airbnb clone')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addTag('airbnb')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
