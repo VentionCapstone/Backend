485672deed05e4d9a84355386a4c6664c9d840fa
Zach13131 <125944644+Zach13131@users.noreply.github.com>
2023-12-05 22:45:35 +0500

    fix(update): change type of key in global exception

--- src/exceptions/global.exception.ts
+++ src/exceptions/global.exception.ts
@@ -1,7 +1,8 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+import ErrorsTypes from 'src/errors/errors.enum';
 
 export class GlobalException extends HttpException {
-  constructor(key: string) {
+  constructor(key: ErrorsTypes) {
     super(key, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
