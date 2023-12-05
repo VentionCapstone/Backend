644e085ebbd3b5c8ba49718e70a511e32c423289
Zilola <44432264+zilola08@users.noreply.github.com>
2023-12-05 17:06:07 +0500

    feat(amenities): create migration for changes in amenities

--- prisma/migrations/20231201064755_add_unique_to_accomid_in_amenity/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[accommodationId]` on the table `Amenity` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Amenity_accommodationId_key" ON "Amenity"("accommodationId");
