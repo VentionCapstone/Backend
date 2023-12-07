38b6a4c85fed35f9d0052fc0d0b3272eca6f3b91
Sinumi <muniss138@gmail.com>
2023-12-07 14:45:40 +0500

    fix: add error handling to prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { UiTheme } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -15,115 +15,134 @@ export class UserService {
   }
 
   async getUser(id: string) {
-    const user = await this.prismaService.user.findUnique({
-      where: { id },
-      include: { Profile: true },
-    });
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { id },
+        include: { Profile: true },
+      });
 
-    if (!user) {
-      throw new NotFoundException(`User with ID ${id} not found`);
+      if (!user) {
+        throw new NotFoundException(`User with ID ${id} not found`);
+      }
+
+      return user;
+    } catch (error) {
+      throw new HttpException('Failed to get user.', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    return user;
   }
 
   async getUserProfile(id: string) {
-    const userProfile = await this.prismaService.userProfile.findFirst({
-      where: { id },
-    });
+    try {
+      const userProfile = await this.prismaService.userProfile.findFirst({
+        where: { id },
+      });
 
-    if (!userProfile) {
-      throw new NotFoundException(`User Profile with ID ${id} not found`);
+      if (!userProfile) {
+        throw new NotFoundException(`User Profile with ID ${id} not found`);
+      }
+
+      return userProfile;
+    } catch (error) {
+      throw new HttpException('Failed to get user profile.', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    return userProfile;
   }
 
   async createUserProfile(createUserDto: CreateUserDto) {
-    const user = await this.prismaService.user.findUnique({
-      where: { id: createUserDto.userId },
-      include: { Profile: true },
-    });
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { id: createUserDto.userId },
+        include: { Profile: true },
+      });
 
-    if (!user) {
-      throw new NotFoundException(`User with ID ${createUserDto.userId} not found`);
-    } else if (user.Profile) {
-      throw new NotFoundException(`User Profile with ID ${user.Profile.id} already exists`);
-    }
+      if (!user) {
+        throw new NotFoundException(`User with ID ${createUserDto.userId} not found`);
+      } else if (user.Profile) {
+        throw new NotFoundException(`User Profile with ID ${user.Profile.id} already exists`);
+      }
 
-    // update user
-    await this.prismaService.user.update({
-      where: { id: createUserDto.userId },
-      data: {
-        firstName: createUserDto.firstName,
-        lastName: createUserDto.lastName,
-      },
-    });
+      // update user
+      await this.prismaService.user.update({
+        where: { id: createUserDto.userId },
+        data: {
+          firstName: createUserDto.firstName,
+          lastName: createUserDto.lastName,
+        },
+      });
 
-    const data = {
-      phoneNumber: createUserDto.phoneNumber,
-      photoUrl: createUserDto.photoUrl,
-      gender: createUserDto.gender,
-      country: createUserDto.country,
-      language: createUserDto.language,
-      uiTheme: createUserDto.uiTheme as UiTheme,
-      description: createUserDto.description,
-    };
+      const data = {
+        phoneNumber: createUserDto.phoneNumber,
+        photoUrl: createUserDto.photoUrl,
+        gender: createUserDto.gender,
+        country: createUserDto.country,
+        language: createUserDto.language,
+        uiTheme: createUserDto.uiTheme as UiTheme,
+        description: createUserDto.description,
+      };
 
-    // create user profile
-    await this.prismaService.userProfile.create({
-      data: {
-        ...data,
-        user: {
-          connect: {
-            id: createUserDto.userId,
+      return await this.prismaService.userProfile.create({
+        data: {
+          ...data,
+          user: {
+            connect: {
+              id: createUserDto.userId,
+            },
           },
         },
-      },
-    });
+      });
+    } catch (error) {
+      throw new HttpException('Failed to create user.', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async updateUserProfile(id: string, updateUserDto: UpdateUserDto) {
-    if (!updateUserDto.userId) {
-      throw new NotFoundException(`User with ID ${updateUserDto.userId} not found`);
+    try {
+      if (!updateUserDto.userId) {
+        throw new NotFoundException(`User with ID ${updateUserDto.userId} not found`);
+      }
+
+      const userProfile = await this.prismaService.userProfile.findFirst({
+        where: { id },
+      });
+
+      if (!userProfile) {
+        throw new NotFoundException(`User Profile with ID ${id} not found`);
+      }
+
+      // update user
+      await this.prismaService.user.update({
+        where: { id: updateUserDto.userId },
+        data: {
+          firstName: updateUserDto.firstName,
+          lastName: updateUserDto.lastName,
+        },
+      });
+
+      const data = {
+        phoneNumber: updateUserDto.phoneNumber,
+        photoUrl: updateUserDto.photoUrl,
+        gender: updateUserDto.gender,
+        country: updateUserDto.country,
+        language: updateUserDto.language,
+        uiTheme: updateUserDto.uiTheme as UiTheme,
+        description: updateUserDto.description,
+      };
+
+      return await this.prismaService.userProfile.update({
+        where: { id },
+        data: data,
+      });
+    } catch (error) {
+      throw new HttpException('Failed to update user.', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    const userProfile = await this.prismaService.userProfile.findFirst({
-      where: { id },
-    });
-
-    if (!userProfile) {
-      throw new NotFoundException(`User Profile with ID ${id} not found`);
-    }
-
-    // update user
-    await this.prismaService.user.update({
-      where: { id: updateUserDto.userId },
-      data: {
-        firstName: updateUserDto.firstName,
-        lastName: updateUserDto.lastName,
-      },
-    });
-
-    const data = {
-      phoneNumber: updateUserDto.phoneNumber,
-      photoUrl: updateUserDto.photoUrl,
-      gender: updateUserDto.gender,
-      country: updateUserDto.country,
-      language: updateUserDto.language,
-      uiTheme: updateUserDto.uiTheme as UiTheme,
-      description: updateUserDto.description,
-    };
-
-    return await this.prismaService.userProfile.update({
-      where: { id },
-      data: data,
-    });
   }
 
   async removeUserProfile(id: string) {
-    return await this.prismaService.userProfile.delete({
-      where: { id },
-    });
+    try {
+      return await this.prismaService.userProfile.delete({
+        where: { id },
+      });
+    } catch (error) {
+      throw new HttpException('Failed to delete user.', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
