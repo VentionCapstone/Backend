12afba8dc3d90bff4ca40224af719070a0f9dab9
Zilola <44432264+zilola08@users.noreply.github.com>
2024-01-04 12:48:25 +0500

    feat(forgot-password): add dtos

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -2,3 +2,5 @@ export * from './login-auth.dto';
 export * from './register-auth.dto';
 export * from './email-verification.dto';
 export * from './email-update.dto';
+export * from './forgot-password.dto';
+export * from './setNewPassword.dto';
---
+++ src/auth/dto/setNewPassword.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsStrongPassword, MinLength } from 'class-validator';
+
+export class SetNewPasswordDto {
+  @ApiProperty({ description: 'token' })
+  @IsNotEmpty()
+  @IsString()
+  token: string;
+
+  @ApiProperty({ example: 'Qwerty!2345', description: 'new password' })
+  @MinLength(8)
+  @IsStrongPassword()
+  newPassword: string;
+
+  @ApiProperty({ example: 'Qwerty!2345', description: 'new password' })
+  @MinLength(8)
+  @IsStrongPassword()
+  confirmPassword: string;
+}
