861c66ca0e41253e3f63ec4d8f844eb1acd091dd
Shahroz Gulliyev <safyur0621@gmail.com>
2024-01-04 17:21:54 +0500

    fix(update): delete option

--- src/payment/dto/confirm-payment.dto.ts
+++ src/payment/dto/confirm-payment.dto.ts
@@ -1,11 +1,6 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { PaymentOption } from './create-payment.dto';
 
 export class ConfirmPaymentDto {
-  @IsNotEmpty()
-  @IsString()
-  paymentOption: PaymentOption;
-
   @IsNotEmpty()
   @IsString()
   bookingId: string;
