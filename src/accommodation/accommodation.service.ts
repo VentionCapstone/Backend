c1647b0aeed9bc7cbd131fba08f1b17eaf0e355e
Zilola <44432264+zilola08@users.noreply.github.com>
2024-01-12 14:41:46 +0500

    Revert "refactor(search): update parsing func"
    This reverts commit 4db728e15255832dd30e1c24c7a180b5fbd09280.

--- src/accommodation/accommodation.service.ts
+++ src/accommodation/accommodation.service.ts
@@ -321,7 +321,7 @@ export class AccommodationService {
   private parseAddress(addressString: string) {
     const addressComponents = addressString.split(',').map((component) => component.trim());
 
-    const [country, city, street] = addressComponents;
+    const [country, city, street] = addressComponents.reverse();
     return { country, city, street };
   }
 
