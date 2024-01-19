690c563903ef7e9d20ad1551adb0ec89041f9fed
Zach13131 <125944644+Zach13131@users.noreply.github.com>
2024-01-19 22:36:30 +0500

    feat(add): google login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,20 +5,20 @@ import { UserGuard } from '../common/guards/user.guard';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { GoogleController } from './google/google.controller';
+// import { GoogleController } from './google/google.controller';
 import { GoogleService } from './google/google.service';
-import { GoogleStrategy } from './google/google.strategy';
+// import { GoogleStrategy } from './google/google.strategy';
 import { PasswordService } from './google/password.service';
 import { VerificationSerivce } from './verification.service';
 
 @Module({
   imports: [PrismaModule, JwtModule.register({}), MailerModule],
-  controllers: [AuthController, GoogleController],
+  controllers: [AuthController],
   providers: [
     AuthService,
     UserGuard,
     VerificationSerivce,
-    GoogleStrategy,
+    // GoogleStrategy,
     GoogleService,
     PasswordService,
   ],
--- src/auth/google/google.service.ts
+++ src/auth/google/google.service.ts
@@ -1,61 +1,87 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import { Response } from 'express';
+import { OAuth2Client } from 'google-auth-library';
 import { MailerService } from 'src/mailer/mailer.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthService } from '../auth.service';
 import { PasswordService } from './password.service';
 
-const { FRONTEND_URL, SALT_LENGTH } = process.env;
+const { SALT_LENGTH, GOOGLE_CLIENT_ID, GOOGLE_SECRET } = process.env;
 
 const SaltLength = parseInt(SALT_LENGTH!);
+
+const client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_SECRET);
 
 @Injectable()
 export class GoogleService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly passwordService: PasswordService,
-    private readonly mailerService: MailerService
+    private readonly mailerService: MailerService,
+    private readonly authService: AuthService
   ) {}
-  async googleLogin(user: any): Promise<string> {
-    try {
-      if (!user) {
-        //change
-        return 'No user from google';
-      }
-      const { email, firstName, lastName, isEmailVerified } = user;
-      console.log('GoogleService ~ googleLogin ~ req:', user);
+  async googleLogin(token: string, res: Response) {
+    console.log('GoogleService ~ googleLogin ~ token:', token);
 
-      const userFromDb = await this.prismaService.user.findUnique({
-        where: { email },
-      });
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-      if (!userFromDb) {
-        const password = this.passwordService.generatePassword();
-        const hashedPassword: string = await bcrypt.hash(password, SaltLength);
+    const user = ticket.getPayload();
 
-        const savedUser = await this.prismaService.user.create({
-          data: {
-            email,
-            password: hashedPassword,
-            firstName,
-            lastName,
-            isEmailVerified,
-          },
-        });
-        console.log('GoogleService ~ googleLogin ~ savedUser:', savedUser);
-
-        await this.mailerService.sendHtmlEmail(
-          email,
-          'Your credentials',
-          this.generateEmailBody(email, password)
-        );
-      }
-
-      return `${FRONTEND_URL}/account/create`;
-    } catch (err) {
-      console.log('GoogleService ~ googleLogin ~ err:', err);
-
-      return `${FRONTEND_URL}/auth/signin`;
+    if (!user) {
+      return 'ERORR HERE';
     }
+    const {
+      email,
+      sub,
+      given_name: firstName,
+      family_name: lastName,
+      email_verified: isEmailVerified,
+    } = user;
+
+    if (!email) {
+      console.error('Email not available.');
+      return 'ERROR HERE';
+    }
+    let userFromDb = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    const password = this.passwordService.generatePassword();
+
+    if (!userFromDb) {
+      const hashedPassword: string = await bcrypt.hash(password, SaltLength);
+
+      userFromDb = await this.prismaService.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          firstName,
+          lastName,
+          isEmailVerified,
+          authSocialSign: sub,
+          authType: 'GOOGLE',
+        },
+      });
+      console.log('GoogleService ~ googleLogin ~ savedUser:', userFromDb);
+
+      await this.mailerService.sendHtmlEmail(
+        email,
+        'Your credentials',
+        this.generateEmailBody(email, password)
+      );
+    }
+
+    const tokens = await this.authService.getTokens(userFromDb.id, email, userFromDb.role);
+    console.log('GoogleService ~ googleLogin ~ data:', tokens);
+
+    const setRefresh = this.authService.setRefreshTokenCookie(tokens.refresh_token, res);
+    console.log('GoogleService ~ googleLogin ~ setRefresh:', setRefresh);
+    throw new Error('from back');
+
+    // return { tokens, id: userFromDb.id };
   }
 
   private generateEmailBody(email: string, password: string): string {
